9cc6d9eaccc8055d5760ce9e56850c56fa861b66
unknown <skooter500>
2015-02-04 15:13:50 +0000

    Bla

--- Java Examples/MatrixWIP/ie/dit/Main.java
+++ Java Examples/MatrixWIP/ie/dit/Main.java
@@ -4,24 +4,26 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Matrix2D m = new Matrix2D(5, 3);
-		
-		m.setCell(3, 2, 5);
-		m.identity();
-		
-		
 		Matrix2D a = new Matrix2D(4, 4);
 		a.identity();
+		a.setCell(2, 3, 7);
+		a.setCell(3, 1, 2);
+		a.setCell(3, 0, 4);
 		
 		Matrix2D b = new Matrix2D(4, 4);
 		b.identity();
+		b.setCell(2, 3, 1);
+		b.setCell(3, 1, 9);
+		b.setCell(3, 0, -7);
 		
 		Matrix2D c;
-		c = Matrix2D.add(a, b); // How to call a static method
+		c = Matrix2D.mult(a, b); // How to call a static method
 		
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
+		
+		
 		
 	}
 }
