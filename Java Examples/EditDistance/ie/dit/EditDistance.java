f46055cbffd47e619a99668f60826548280153c1
unknown <skooter500>
2015-02-18 15:41:31 +0000

    Bla

---
+++ Java Examples/EditDistance/ie/dit/EditDistance.java
@@ -0,0 +1,51 @@
+package ie.dit;
+
+public class EditDistance
+{
+	 private static float min3(float a, float b, float c)
+	{
+		if ((a < b) && (a < c))
+		{
+			return a;
+		}
+		if ((b < a) && (b < c))
+		{
+			return b;
+		}
+		return c;
+	}
+
+	public static float MinimumEditDistance(String needle, String haystack)
+	{
+		int rows = needle.length() + 1;
+		int cols = haystack.length() + 1;
+		Matrix2D m = new Matrix2D(rows, cols);
+		
+		for (int row = 0 ; row < rows; row ++)
+		{
+			m.setCell(row, 0, row);
+		}	
+		for (int col = 0 ; col < cols; col ++)
+		{
+			m.setCell(0, col, col);
+		}	
+
+		for (int row = 1 ; row < rows ; row ++)
+		{
+			for (int col = 1 ; col < cols ; col ++)
+			{
+				if (haystack.charAt(col -1) == needle.charAt(row -1))
+				{
+					m.setCell(row, col, m.getCell(row -1 , col -1));
+				}
+				else
+				{
+					float min = min3(m.getCell(row -1 , col -1), m.getCell(row , col -1), m.getCell(row -1 , col ));
+					m.setCell(row, col, min + 1);
+				}
+			}
+		}
+		System.out.println(m);
+		return (int) m.getCell(rows - 1, cols - 1);
+	}
+}
--- Java Examples/EditDistance/ie/dit/Main.java
+++ Java Examples/EditDistance/ie/dit/Main.java
@@ -4,26 +4,13 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Matrix2D a = new Matrix2D(4, 4);
-		a.identity();
-		a.setCell(2, 3, 7);
-		a.setCell(3, 1, 2);
-		a.setCell(3, 0, 4);
-		
-		Matrix2D b = new Matrix2D(4, 4);
-		b.identity();
-		b.setCell(2, 3, 1);
-		b.setCell(3, 1, 9);
-		b.setCell(3, 0, -7);
-		
-		Matrix2D c;
-		c = Matrix2D.mult(a, b); // How to call a static method
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		
-		
-		
+		String sa = "I love DIT";
+		String sb = "I love Tunepal";
+		System.out.println("Edit distance between: " + sa + " and: " + sb + " is " + EditDistance.MinimumEditDistance(sa, sb));
+
+		sa = "Games Fleadh";
+		sb = "Imagine Cup";
+		System.out.println("Edit distance between: " + sa + " and: " + sb + " is " + EditDistance.MinimumEditDistance(sa, sb));
+
 	}
 }
